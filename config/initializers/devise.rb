6fc3bf371425b8054c36bcb532a30763d2093a1c
Henrique Breim <breim@protonmail.ch>
2016-10-18 04:46:11 -0200

    add facebook omniauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,9 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  # Omniauth
+  config.omniauth :facebook, '857046371045214', '82c8ab9b201134cd737d917a01ef56f7'
+
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root 'pages#index'
   get 'pages/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, :controllers => {:omniauth_callbacks => "users/omniauth_callbacks"}
 end
