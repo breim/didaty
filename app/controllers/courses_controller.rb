5b124c5f862b52e37c9d50da06bad89c2229c231
Henrique Breim <breim@protonmail.ch>
2016-10-19 08:31:58 -0200

    Add images to courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,10 +1,14 @@
 class CoursesController < ApplicationController
   before_action :set_course, only: [:show, :edit, :update, :destroy]
+  before_action :validates_user, only: [:edit, :update, :destroy]
+  
+  respond_to :html
 
   # GET /courses
   # GET /courses.json
   def index
-    @courses = Course.all
+     params[:teacher].present? ? 
+     @courses = Course.all.where(deleted: false) : @courses = Course.all
   end
 
   # GET /courses/1
@@ -25,16 +29,9 @@ class CoursesController < ApplicationController
   # POST /courses.json
   def create
     @course = Course.new(course_params)
-
-    respond_to do |format|
-      if @course.save
-        format.html { redirect_to @course, notice: 'Course was successfully created.' }
-        format.json { render :show, status: :created, location: @course }
-      else
-        format.html { render :new }
-        format.json { render json: @course.errors, status: :unprocessable_entity }
-      end
-    end
+    @course.user_id = current_user.id
+    @course.save
+    respond_with(@course, location: course_path(@course))
   end
 
   # PATCH/PUT /courses/1
@@ -54,7 +51,7 @@ class CoursesController < ApplicationController
   # DELETE /courses/1
   # DELETE /courses/1.json
   def destroy
-    @course.destroy
+    @course.update(deleted: true)
     respond_to do |format|
       format.html { redirect_to courses_url, notice: 'Course was successfully destroyed.' }
       format.json { head :no_content }
@@ -69,6 +66,12 @@ class CoursesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def course_params
-      params.require(:course).permit(:name, :subtitle, :description, :price, :active, :deleted, :category_id, :user_id)
+      params.require(:course).permit(:name, :subtitle, :description, :price, :active, :deleted, :category_id, :image)
+    end
+
+    def validates_user
+      unless current_user.present? and @course.user_id == current_user.id
+        redirect_to courses_path; flash[:alert] = 'views.not_loged_or_not_woner'
+      end
     end
 end
